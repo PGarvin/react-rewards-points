4cf8eee2bfc373dc8cca32df7f77c1f0db37839e
PGarvin <pgarvin@gmail.com>
2020-07-02 18:34:31 -0500

    First commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default function returnMusicData() {
+	return [{"Date":"7-01-2020","Spent":120},
+{"Date":"6-24-2020","Spent":210},
+{"Date":"6-21-2020","Spent":104},
+{"Date":"5-21-2020","Spent":67},
+{"Date":"3-29-2020","Spent":22},
+{"Date":"5-16-2020","Spent":88},
+{"Date":"6-09-2020","Spent":132},
+{"Date":"6-28-2020","Spent":40},
+{"Date":"6-21-2020","Spent":252},
+{"Date":"6-18-2020","Spent":125},
+{"Date":"5-18-2020","Spent":80},
+{"Date":"3-26-2020","Spent":26},
+{"Date":"5-13-2020","Spent":106},
+{"Date":"6-06-2020","Spent":158},
+{"Date":"6-25-2020","Spent":48},
+{"Date":"6-18-2020","Spent":302},
+{"Date":"6-15-2020","Spent":150},
+{"Date":"5-15-2020","Spent":96}]
+}
--- src/index.js
+++ src/index.js
@@ -1,181 +1,166 @@
-import React from 'react'
-import ReactDOM, { render } from 'react-dom'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import Table from "react-bootstrap/Table";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import spendingData from "./data";
+
+const months = [
+  { Month: "January", Number: 0, Points: 0, Current: "" },
+  { Month: "February", Number: 1, Points: 0, Current: "" },
+  { Month: "March", Number: 2, Points: 0, Current: "" },
+  { Month: "April", Number: 3, Points: 0, Current: "" },
+  { Month: "May", Number: 4, Points: 0, Current: "" },
+  { Month: "June", Number: 5, Points: 0, Current: "" },
+  { Month: "July", Number: 6, Points: 0, Current: "" },
+  { Month: "August", Number: 7, Points: 0, Current: "" },
+  { Month: "September", Number: 8, Points: 0, Current: "" },
+  { Month: "October", Number: 9, Points: 0, Current: "" },
+  { Month: "November", Number: 10, Points: 0, Current: "" },
+  { Month: "December", Number: 11, Points: 0, Current: "" },
+];
+
+let threeMonthsTally = 0;
+
+const calculatePoints = (moneySpent) => {
+  let points = 0;
+  if (50 < moneySpent && moneySpent <= 100) {
+    points += moneySpent - 50;
+  }
+  if (100 < moneySpent) {
+    points += 50 + (moneySpent - 100) * 2;
+  }
+  return points;
+};
+
+const isInLast3Months = (date) => {
+  let threeMonthStart = new Date();
+  let spendingDate = new Date(date);
+  threeMonthStart.setDate(1);
+  threeMonthStart.setMonth(threeMonthStart.getMonth() - 2);
+
+  return spendingDate - threeMonthStart > 0;
+};
+
+const startingList = spendingData();
+
+const purchaseList = startingList
+  .filter(function (purchase) {
+    if (!isInLast3Months(purchase.Date)) {
+      return false; // skip
+    }
+    return true;
+  })
+  .map(function (purchase) {
+    const purchaseDate = new Date(purchase.Date);
+    purchase.month = purchaseDate.getMonth();
+    purchase.points = calculatePoints(purchase.Spent);
+    months[purchase.month]["Points"] += purchase.points;
+    threeMonthsTally += purchase.points;
+    months[purchase.month]["Current"] = "true";
+    return purchase;
+  });
+
+const threeMonths = months
+  .filter(function (month) {
+    if (month.Current !== "true") {
+      return false; // skip
+    }
+    return true;
+  })
+  .map(function (month) {
+    return month;
+  });
+
+console.log(threeMonths);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-let bookList = [{
-      "Titles": "Diary of a wimpy kid : Greg Heffley's journal",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 63,
-      "Image": "wimp1"
-    },
-    {
-      "Titles": "The catcher in the rye",
-      "Author": "Salinger, J. D.",
-      "ItemsLost": 49,
-      "Image": "RYE"
-    },
-    {
-      "Titles": "The great Gatsby",
-      "Author": "Fitzgerald, F. Scott",
-      "ItemsLost": 47,
-      "Image": "Gatsby"
-    },
-    {
-      "Titles": "A child called 'it' : one child's courage to survive",
-      "Author": "Pelzer, David J.",
-      "ItemsLost": 45,
-      "Image": "IT"
-    },
-    {
-      "Titles": "The autobiography of Malcolm X",
-      "Author": "X, Malcolm,",
-      "ItemsLost": 45,
-      "Image": "AutobiographyOfMalcolmX"
-    },
-    {
-      "Titles": "Diary of a wimpy kid : the third wheel",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 44,
-      "Image": "Wimp2"
-    },
-    {
-      "Titles": "Fifty shades of Grey",
-      "Author": "James, E.L.",
-      "ItemsLost": 44,
-      "Image": "shades"
-    },
-    {
-      "Titles": "The alchemist",
-      "Author": "Coelho, Paulo",
-      "ItemsLost": 43,
-      "Image": "alchemist"
-    },
-    {
-      "Titles": "Diary of a wimpy kid : dog days",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 42,
-      "Image": "wimp3"
-    },
-    {
-      "Titles": "The coldest winter ever : a novel",
-      "Author": "Souljah, Sister",
-      "ItemsLost": 42,
-      "Image": "coldest"
-    },
-    {
-      "Titles": "The fault in our stars",
-      "Author": "Green, John",
-      "ItemsLost": 42,
-      "Image": "Fault"
-    },
-    {
-      "Titles": "Diary of a wimpy kid : the last straw",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 41,
-      "Image": "wimp4"
-    },
-    {
-      "Titles": "All souls : a family story from Southie",
-      "Author": "MacDonald, Michael Patrick",
-      "ItemsLost": 39,
-      "Image": "allSouls"
-    },
-    {
-      "Titles": "Diary of a wimpy kid : hard luck",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 39,
-      "Image": "wimp5"
-    },
-    {
-      "Titles": "The book thief",
-      "Author": "Zusak, Markus",
-      "ItemsLost": 39,
-      "Image": "bookThief"
-    },
-    {
-      "Titles": "The absolutely true diary of a part-time Indian",
-      "Author": "Alexie, Sherman",
-      "ItemsLost": 38,
-      "Image": "partTime"
-    },
-    {
-      "Titles": "Diary of a wimpy kid : cabin fever",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 36,
-      "Image": "wimp6"
-    },
-    {
-      "Titles": "Diary of a wimpy kid : the ugly truth",
-      "Author": "Kinney, Jeff",
-      "ItemsLost": 36,
-      "Image": "wimp7"
-    },
-    {
-      "Titles": "The hunger games",
-      "Author": "Collins, Suzanne",
-      "ItemsLost": 36,
-      "Image": "hungerGames"
-    },
-    {
-      "Titles": "The cat in the hat",
-      "Author": "Seuss, Dr.",
-      "ItemsLost": 35,
-      "Image": "cat"
-    }
-  ];
+purchaseList.sort(function (a, b) {
+  if (a.Date > b.Date) {
+    return -1;
+  }
+  if (a.Date < b.Date) {
+    return 1;
+  }
+  return 0;
+});
 
-const pictures = (number, image) => {
-    var elements = [];
-    for (let i =0; i < number; i++){
-        elements.push(<img src={require("./assets/"+image+".jpg")} alt="" className="book" key={i} />); 
-    }
-    return elements;
+const Purchase = ({ date, spent }) => {
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>${spent}</td>
+      <td>{calculatePoints(spent)}</td>
+    </tr>
+  );
+};
+
+const MonthTally = ({ monthName, points }) => {
+  return (
+    <div className="tally">
+      <p className="monthName">{monthName}</p>
+      <p>{points}</p>
+    </div>
+  );
+};
+
+class MonthCollection extends React.Component {
+  render() {
+    const { months } = this.props;
+    return (
+      <div className="tally_holder">
+        {months.map((month, i) => (
+          <MonthTally
+            key={`month_${i}`}
+            monthName={month.Month}
+            points={month.Points}
+          />
+        ))}
+        <div className="tally">
+          <p className="monthName">Total</p>
+          <p>{threeMonthsTally}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-const Book = ({title, author, items, image}) => {
-	return (
-		<section className="holder">
-			<div className="titleAuthor">
-        		<div className="title">{title}</div>
-        		<div className="author">{author}</div>
-    		</div>
-    		<div className="itemCount">{items}</div>
-  			<div className="imgHolder">{pictures(items, image)}</div>
-		</section>
-	)
+class Collection extends React.Component {
+  render() {
+    const { purchases } = this.props;
+    return (
+      <div className="holder">
+        <h1>Points from the last 3 months</h1>
+        <MonthCollection months={threeMonths} />
+        <Table striped bordered hover size="sm" className="table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Amount Spent</th>
+              <th>Points Earned</th>
+            </tr>
+          </thead>
+          <tbody>
+            {purchases.map((purchase, i) => (
+              <Purchase key={i} date={purchase.Date} spent={purchase.Spent} />
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
-
-class Library extends React.Component {
-	render() {
-		const { books } = this.props
-		return (
-			<div>
-				{books.map(
-					(book, i) =>
-						<Book 
-							key={i}
-							title={book.Titles} 
-							author={book.Author} 
-							items={book.ItemsLost}
-							image={book.Image}/>
-				)}
-			</div>
-		)
-	}
-}
-
-
 
 render(
-	<Library books={bookList}/>, 
-	document.getElementById('root')
-)
+  <Collection purchases={purchaseList} />,
+  document.getElementById("root")
+);
