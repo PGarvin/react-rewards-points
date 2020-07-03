3eb0c1f3d8b645578029c84a58b56202890e577e
PGarvin <pgarvin@gmail.com>
2020-07-03 00:16:22 -0500

    Moved components to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
 
-function App() {
+import spendingData from "./JSON/data";
+import monthsData from "./JSON/months";
+
+const startingList = spendingData();
+const months = monthsData();
+
+let threeMonthsTally = 0;
+
+const calculatePoints = require("./functions.js").calculatePoints;
+const isInLast3Months = require("./functions.js").isInLast3Months;
+
+const purchaseList = startingList
+  .filter(function (purchase) {
+    if (!isInLast3Months(purchase.Date)) {
+      return false; // skip
+    }
+    return true;
+  })
+  .map(function (purchase) {
+    const purchaseDate = new Date(purchase.Date);
+    purchase.month = purchaseDate.getMonth();
+    purchase.points = calculatePoints(purchase.Spent);
+    months[purchase.month]["Points"] = parseInt(months[purchase.month]["Points"]);
+    months[purchase.month]["Points"] += Number(purchase.points);
+    threeMonthsTally += purchase.points;
+    months[purchase.month]["Current"] = "true";
+    return purchase;
+  });
+
+const threeMonths = months
+  .filter(function (month) {
+    if (month.Current !== "true") {
+      return false; // skip
+    }
+    return true;
+  })
+  .map(function (month) {
+    return month;
+  });
+
+
+purchaseList.sort(function (a, b) {
+  if (a.Date > b.Date) {
+    return -1;
+  }
+  if (a.Date < b.Date) {
+    return 1;
+  }
+  return 0;
+});
+
+const Purchase = ({ date, spent }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{date}</td>
+      <td>${spent}</td>
+      <td>{calculatePoints(spent)}</td>
+    </tr>
+  );
+};
+
+const MonthTally = ({ monthName, points }) => {
+  return (
+    <div className="tally">
+      <p className="monthName">{monthName}</p>
+      <p>{points}</p>
     </div>
   );
+};
+
+class MonthCollection extends React.Component {
+  render() {
+    const { months } = this.props;
+    return (
+      <div className="tally_holder">
+        {months.map((month, i) => (
+          <MonthTally
+            key={`month_${i}`}
+            monthName={month.Month}
+            points={month.Points}
+          />
+        ))}
+        <div className="tally">
+          <p className="monthName">Total</p>
+          <p>{threeMonthsTally}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Collection extends React.Component {
+  render() {
+    const { purchases } = this.props;
+    return (
+      <div className="holder">
+        <h1>Points from the last 3 months</h1>
+        <MonthCollection months={threeMonths} />
+        <Table striped bordered hover size="sm" className="table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Amount Spent</th>
+              <th>Points Earned</th>
+            </tr>
+          </thead>
+          <tbody>
+            {purchases.map((purchase, i) => (
+              <Purchase key={i} date={purchase.Date} spent={purchase.Spent} />
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+
+
+class App extends Component {
+  render() {
+  return (
+    <Collection purchases={purchaseList} />
+  );
+}
 }
 
 export default App;
